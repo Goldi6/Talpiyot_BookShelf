2acae983b533aba7cf963ee78098b5278853ed8f
Goldi6 <nextdraw.arts@gmail.com>
2023-01-28 06:42:34 +0200

    unused files removed

--- public/script/handlebars-script/cart.js
+++ public/script/handlebars-script/cart.js
@@ -4,29 +4,6 @@ import {
   setupCheckoutBtn,
 } from "./modules/cartActions.js";
 
-// import { verifyModal } from "./modules/verifyPurchase.js";
-
-// window.onload = function () {
-//   getCart();
-// };
-
-// const getCart = async () => {
-//   let cart = [];
-//   if (getToken() !== "") {
-//     //const token = getToken();
-//     getUserCart().then((cart) => {
-//       console.log(cart);
-//       setupCartItems(cart);
-//     });
-//   } else {
-//     if (localStorage.getItem("cart")) {
-//       getLocalCart(localStorage.getItem("cart")).then((cart) => {
-//         setupCartItems(cart);
-//       });
-//     }
-//   }
-//   return cart;
-// };
 setupCheckoutBtn();
 setupDeleteButtons();
 setupUpdateButtons();
